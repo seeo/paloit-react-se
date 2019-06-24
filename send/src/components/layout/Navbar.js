2ac0fff87450001c7545d3106d0582dd15a9b031
Christophe Eo Siang Ee <aragonlotr5@gmail.com>
2019-06-25 07:14:10 +0800

    working on PALO IT react pic order form again

---
+++ send/src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import logo from '../../images/logo.png';
+
+const Navbar = ({logo_description, title}) => {
+    //allow site to display the default props, if no props are specified at the parent
+
+    return(
+        <div className = "navbar">
+            <img
+                src = {logo}
+                alt = {logo_description}
+            />
+            <h3> {title}</h3>
+
+        </div>
+    )
+}
+
+    Navbar.defaultProps = {
+        title: 'PALOPIC Order System',
+        // logo: '../../../public/images/logo.png',
+        logo_description: "PALO IT's EPIC logo",
+    };
+      //type checking...
+    Navbar.propTypes = {
+        title: PropTypes.string.isRequired,
+        logo_description: PropTypes.string.isRequired,
+    }
+
+export default Navbar;
--- send/src/components/main/OrderSummary.js
+++ send/src/components/main/OrderSummary.js
@@ -7,7 +7,7 @@ export class OrderSummary extends Component {
                 <h3>Summary of your order:</h3>
                 <div>
                     Last name:
-                    <p type = "text" name = "lastName" value={this.props.lastName} />
+                    <p type="text" name="lastName" value={this.props.lastName} />
                 </div>
             </div>
         )
